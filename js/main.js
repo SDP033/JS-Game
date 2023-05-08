0192465d5d9b1f29e0dcb3500cbe9d0cb9570c48
SDP033 <sergiodelgadoprada@gmail.com>
2023-05-08 19:42:25 +0200

    Pintar Rectangulo

--- js/main.js
+++ js/main.js
@@ -3,11 +3,23 @@ let contexto = canvas.getContext("2d");       // Hacemos referencia al canvas y 
 
 
 
-var x=canvas.width / 2;
+var x=canvas.width / 2;        // Pasos para crear la pelota
 var y=canvas.height- 50;
 var radio = 10;
 contexto.beginPath();
+contexto.lineWidth = 5; 
+contexto.strokeStyle = "#212121";
 contexto.arc(x, y, radio, 0, 2 * Math.PI);
 contexto.fillStyle= "blue";
+contexto.stroke();
+contexto.fill();
+contexto.closePath();
+
+contexto.beginPath();
+contexto.border = 5;             // Dibujamos un rectángulo 
+contexto.strokeStyle = "#212121";
+contexto.fillStyle = "#F4511E";
+contexto.rect(canvas.width/2, canvas.height-30, 100, 25);
+contexto.stroke();
 contexto.fill();
 contexto.closePath();
