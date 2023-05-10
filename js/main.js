8700ab115a7d74c82e3439f3a643e1a18b42adb8
SDP033 <sergiodelgadoprada@gmail.com>
2023-05-10 10:59:11 +0200

    Moviendo la pelota

--- js/main.js
+++ js/main.js
@@ -1,25 +1,67 @@
-let canvas= document.getElementById("arkanoidCanvas");
-let contexto = canvas.getContext("2d");       // Hacemos referencia al canvas y le damos un contexto de 2 dimensiones para poder pintar los elementos del juego
+let canvas = document.getElementById("arkanoidCanvas");
+let contexto = canvas.getContext("2d"); 
 
-
-
-var x=canvas.width / 2;        // Pasos para crear la pelota
-var y=canvas.height- 50;
+var x = canvas.width / 2; 
+var y = canvas.height - 50;      // Variables pelota
 var radio = 10;
-contexto.beginPath();
+
+var raquetax= (canvas.width / 2)-50;
+var raquetay=canvas.height-40;
+var raquetaw=100;
+var raquetah=25;
+
+var movx= 2;
+var movy=-2;
+
+function pelota(){
+contexto.beginPath();                    // Aqui pintamos un pelota
 contexto.lineWidth = 5; 
 contexto.strokeStyle = "#212121";
 contexto.arc(x, y, radio, 0, 2 * Math.PI);
-contexto.fillStyle= "blue";
+contexto.fillStyle = "blue";
 contexto.stroke();
 contexto.fill();
 contexto.closePath();
 
-contexto.beginPath();
-contexto.border = 5;             // Dibujamos un rectángulo 
+}
+
+function raqueta(){
+    contexto.beginPath();                   // Aqui pintamos la raqueta
+    contexto.lineWidth = 5; // en lugar de "border"
+    contexto.strokeStyle = "#212121";
+    contexto.fillStyle = "#F4511E";
+    contexto.rect(raquetax, raquetay, raquetaw, raquetah);
+    contexto.stroke();
+    contexto.fill();
+    contexto.closePath();
+
+}
+
+function pinta(){       // esta funcion limpia el canvas para poder repintar los elementos que pintamos
+    contexto.clearRect(0, 0, canvas.width, canvas.height);
+    raqueta();
+    pelota();                                                         // Aqui hacemos la logica para que la pelota no sobrepase el canvas
+    if(x + movx > canvas.width - radio || x + movx < radio){
+        movx = -movx;                                                 
+    }
+    if(y + movy < radio || y + movy > canvas.height - radio){
+        movy = -movy;
+    }
+    x += movx;
+    y += movy;
+
+}
+setInterval(pinta, 10);  // Aqui pintamos cada 10 ms todos los elementos que metamos en la funcion pinta
+
+
+
+/*
+
+contexto.beginPath();                  // Aqui pintamos un brick
+contexto.lineWidth = 5; // en lugar de "border"
 contexto.strokeStyle = "#212121";
-contexto.fillStyle = "#F4511E";
-contexto.rect(canvas.width/2, canvas.height-30, 100, 25);
+contexto.fillStyle = "yellow";
+contexto.rect(5, 5, 50, 25);
 contexto.stroke();
 contexto.fill();
-contexto.closePath();
+contexto.closePath();*/
